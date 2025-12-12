cfd86759732316d06723f5c35976ffb4b702701c
shaahzaib <110594965+shaahzaib@users.noreply.github.com>
2025-12-12 14:04:59 +0500

    search logic applied

---
+++ NotesApp/Views/NoteRowView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteRowView.swift
+//  NotesApp
+//
+//  Created by Macbook Pro on 12/12/2025.
+//
+
+import SwiftUI
+
+struct NoteRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteRowView()
+}
